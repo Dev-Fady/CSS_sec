c653cbaa0c1269b3a53735e4ee4670b95d7685fc
fady emil <fadymobiledev@gmail.com>
2025-02-23 18:00:59 +0200

    Add caesar_cipher_decrypt.py for basic Caesar cipher decryption

---
+++ section_2/caesar_cipher_decrypt.py
@@ -0,0 +1,11 @@
+text='Qzuipo'
+k=1
+result=""
+for i in range(len(text)):
+    char = text[i]
+    if(char.isupper()):
+        s = chr((ord(char) - 65 - k) % 26 + 65)
+    else:
+        s=chr((ord(char) - 97 - k ) % 26 + 97)
+    result+=s
+print("Decrypted text:",result)
