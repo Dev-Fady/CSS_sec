f827802b0ce1e61cea5ea566e279849bc475a405
fady emil <fadymobiledev@gmail.com>
2025-02-23 18:00:35 +0200

    Add Basic.py with string manipulation examples and ASCII conversions

---
+++ section_2/Basic.py
@@ -0,0 +1,15 @@
+import string
+
+#~ integer 65 to ASCII
+print(chr(65))
+# ~ ASCII to integer 65
+print(ord("A"))
+
+str_name="string"
+print(str_name.islower())
+print(str_name.isupper())
+print(str_name.upper())
+
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.ascii_letters)
