317290a0627bc4bb96776a411bd809488f6312d7
fady emil <fadymobiledev@gmail.com>
2025-03-03 19:13:50 +0200

    Refactor password handling in simple_password.py for improved clarity and structure

--- task_lecture_2/simple_password.py
+++ task_lecture_2/simple_password.py
@@ -1,27 +1,22 @@
 import bcrypt
 
-# تخزين كلمة المرور بعد تشفيرها
 def hash_password(password):
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
 
-# التحقق من كلمة المرور المدخلة
 def check_password(stored_hash, entered_password):
     return bcrypt.checkpw(entered_password.encode(), stored_hash)
 
 
 users = {}
-# تسجيل مستخدم جديد
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-# تخزين اسم المستخدم مع كلمة المرور المشفرة
 users[username] = hash_password(password)
 
 print("User registered successfully!")
 
-# تسجيل الدخول
 username = input("\nEnter username to login: ")
 password = input("Enter password: ")
 
