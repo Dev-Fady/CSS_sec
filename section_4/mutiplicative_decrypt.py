b2e95c9b115535f758825c43c2e0a7de011c2041
fady emil <fadymobiledev@gmail.com>
2025-04-08 15:57:47 +0200

    Refactor decrypt function to accept key parameter for multiplicative decryption

--- section_4/mutiplicative_decrypt.py
+++ section_4/mutiplicative_decrypt.py
@@ -1,15 +1,16 @@
-def decrypt(cipher):
+def decrypt(cipher,key):
     result=""
     for i in cipher:
         if(i!=""):
             if(i.isupper()):
-                s=chr(((ord(i) - 65) * 3) % 26 + 65)
+                s=chr(((ord(i) - 65) * pow(key,-1,26)) % 26 + 65)
             else:
-                s=chr(((ord(i) - 97) * 3) % 26 + 97)
+                s=chr(((ord(i) - 97) * pow(key,-1,26) ) % 26 + 97)
         else:
             s=" "
         result+=s 
     return result
 message = "Gksxkp"
-plaintext = decrypt(message)
+key=9
+plaintext = decrypt(message,key)
 print("The decrypted text is: " + plaintext)
